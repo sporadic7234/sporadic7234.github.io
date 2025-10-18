d70b5533e0c0cf494836e40cd29c1a25c0006dee
sporadic7234 <159210079+sporadic7234@users.noreply.github.com>
2025-10-18 00:50:02 -0700

    new update

--- tangent/Tangent.service.worker.js
+++ tangent/Tangent.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1760765487|7916858219';
+const CACHE_VERSION = '1760773582|2424302081';
 /** @type {string} */
 const CACHE_PREFIX = 'Tangent-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
